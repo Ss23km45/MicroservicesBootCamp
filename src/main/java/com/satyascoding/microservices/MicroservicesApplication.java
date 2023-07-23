2db3ada517a681fb9255a48e5e0810cb58eaf55f
Satyas <surya1999challa@gmail.com>
2023-07-23 16:54:02 +0530

    #US Playing with Banners in Spring boot

--- src/main/java/com/satyascoding/microservices/MicroservicesApplication.java
+++ src/main/java/com/satyascoding/microservices/MicroservicesApplication.java
@@ -1,13 +1,20 @@
 package com.satyascoding.microservices;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class MicroservicesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MicroservicesApplication.class, args);
+//		SpringApplication.run(MicroservicesApplication.class, args);
+
+		//Another Way of starting Spring application
+		new SpringApplicationBuilder(MicroservicesApplication.class)
+				.bannerMode(Banner.Mode.CONSOLE)
+				.run(args);
 	}
 
 }
